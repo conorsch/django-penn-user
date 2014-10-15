821dceafb96f60023f9faff1c902edf28d16716e
FlipperPA <FlipperPA@users.noreply.github.com>
2014-10-15 17:43:30 -0400

    Fix typos, update URL.

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-penn_user',
+    name='penn_user',
     version='0.1',
     packages=['penn_user'],
     include_package_data=True,
     license='BSD License',
-    description="A simple Django app to use the University of Pennsylvania''s CoSign auth and extend Django's base user.",
+    description="A simple Django app to use the University of Pennsylvania's CoSign auth and extend Django's base user.",
     long_description=README,
-    url='http://stash.wharton.upenn.edu/',
+    url='https://github.com/wharton/django-penn-user/',
     author='Timothy Allen, the Wharton School',
     author_email='tallen@wharton.upenn.edu',
     classifiers=[
