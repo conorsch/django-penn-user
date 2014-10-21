1938d97ea6baee6dd1eada899b5d456a7a7cc0df
FlipperPA <FlipperPA@users.noreply.github.com>
2014-10-21 11:18:20 -0400

    penn_user -> pennuser

--- pennuser/urls.py
+++ pennuser/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from django_penn_user.views import IndexPage
+from pennuser.views import IndexPage
 
 urlpatterns = patterns('',
     url(r'^$', IndexPage.as_view(), name='home'),
