09e11091fd07f47e66732d4ecc448f32b1188a16
FlipperPA <FlipperPA@users.noreply.github.com>
2014-10-31 16:58:40 -0400

    Add defaults to PennUser model.

--- pennuser/models.py
+++ pennuser/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User, UserManager
 
 class PennUser(User):
     penn_id = models.IntegerField()
-    join_date = models.DateField()
+    join_date = models.DateField(default=datetime.now)
     last_login_date = models.DateField()
-    login_count = models.IntegerField()
+    login_count = models.IntegerField(default=0)
     # put additional properties here
 
     objects = UserManager()
