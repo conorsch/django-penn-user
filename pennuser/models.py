f001e6155f7ecd84990b8fde89bc68ecd29bbb1f
FlipperPA <FlipperPA@users.noreply.github.com>
2014-11-07 14:23:14 -0500

    Remove field, keep barebones.

--- pennuser/models.py
+++ pennuser/models.py
@@ -3,12 +3,9 @@ from django.contrib.auth.models import User, UserManager
 
 
 class PennUser(User):
-    penn_id = models.IntegerField()
     join_date = models.DateField(default=datetime.now)
     last_login_date = models.DateField()
     login_count = models.IntegerField(default=0)
     # put additional properties here
 
     objects = UserManager()
-
-# other models...
