a62c7490600eadb80947a2e729eb429e646d2cbe
FlipperPA <FlipperPA@users.noreply.github.com>
2014-11-07 16:01:38 -0500

    Increase version number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='pennuser',
-    version='0.1',
+    version='0.2',
     packages=['pennuser'],
     include_package_data=True,
     license='BSD License',
