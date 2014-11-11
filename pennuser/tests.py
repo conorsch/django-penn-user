dd872ade1b31b2402c163661e18a82039ac9f1f3
Conor Schaefer <conor.schaefer@gmail.com>
2014-11-11 12:04:20 -0500

    adds a slew of tests

--- pennuser/tests.py
+++ pennuser/tests.py
@@ -1,3 +1,51 @@
+#!/usr/bin/env python
 from django.test import TestCase
+from .factories import PennUserFactory, PennUserStaffFactory, PennUserAdminFactory
+from django.core.validators import validate_email
+import re
 
-# Create your tests here.
+
+class TestPennUserAttributes(TestCase):
+
+    def setUp(self):
+        self.user = PennUserFactory()
+
+    def test_default_user_permissions(self):
+        """User should NOT be staff or admin by default."""
+        self.assertFalse(self.user.is_staff)
+        self.assertFalse(self.user.is_admin)
+
+    def test_pennkey_is_valid(self):
+        """Ensure username field is 3-8 alphanumeric characters."""
+        # There should be NO capital letters.
+        self.assertFalse(re.match(r'[A-Z]', self.user.username))
+        self.assertTrue(re.match(r'[a-z0-9]{3,8}', self.user.username))
+
+    def test_email_is_valid(self):
+        """Ensure email address looks right."""
+        # Django's core validator will raise an exception on failure; let's make sure.
+        from django.core.exceptions import ValidationError
+        self.assertRaises(ValidationError, validate_email, ["xxx"])
+
+        # Email address should be valid.
+        validate_email(self.user.email)
+
+
+class TestPennUserStaffAttributes(TestCase):
+
+    def setUp(self):
+        self.user = PennUserStaffFactory()
+
+    def test_user_is_staff(self):
+        self.assertTrue(self.user.is_staff)
+        self.assertFalse(self.user.is_admin)
+
+
+class TestPennUserAdminAttributes(TestCase):
+
+    def setUp(self):
+        self.user = PennUserAdminFactory()
+
+    def test_user_is_admin(self):
+        self.assertTrue(self.user.is_admin)
+        self.assertTrue(self.user.is_staff)
