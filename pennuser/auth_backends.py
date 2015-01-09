7705942ee5009629027d597d57508d377c4b596d
Conor Schaefer <conor.schaefer@gmail.com>
2015-01-08 19:27:31 -0500

    much terser custom auth backend

--- pennuser/auth_backends.py
+++ pennuser/auth_backends.py
@@ -1,40 +1,23 @@
-from django.conf import settings
 from django.contrib.auth.backends import RemoteUserBackend
-from django.core.exceptions import ImproperlyConfigured
-from django.db.models import get_model
+from django.contrib.auth import get_user_model
+
 
 class PennUserModelBackend(RemoteUserBackend):
     def authenticate(self, remote_user):
-        """
-        This code is the same as the authenticate function from 
-        django.contrib.auth.backends.RemoteUserBackend,
-        except with a way to specify our extended User model.  
-        The idea for the self.user_class property comes 
-        from Scott Barnham.
-        """
+        """Cosign will handle password, we just need REMOTE_USER set."""
         if not remote_user:
             return
         user = None
+        user_model = get_user_model()
         username = self.clean_username(remote_user)
 
         if self.create_unknown_user:
-            user, created = self.user_class.objects.get_or_create(username=username)
+            user, created = user_model.objects.get_or_create(username=username)
             if created:
                 user = self.configure_user(user)
         else:
             try:
-                user = self.user_class.objects.get(username=username)
-            except self.user_class.DoesNotExist:
+                user = user_model.objects.get(username=username)
+            except self.user_model.DoesNotExist:
                 pass
         return user
-
-    @property
-    def user_class(self):
-        """
-        This code was written by Scott Barnham.
-        """
-        if not hasattr(self, '_user_class'):
-            self._user_class = get_model(*settings.CUSTOM_USER_MODEL.split('.', 2))
-            if not self._user_class:
-                raise ImproperlyConfigured('Could not get custom user model')
-        return self._user_class
