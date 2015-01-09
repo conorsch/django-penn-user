c31ba50a5f4e0b911b2dc5877e425c1014c5db5f
Conor Schaefer <conor.schaefer@gmail.com>
2015-01-08 19:01:02 -0500

    more concise factories

--- pennuser/factories.py
+++ pennuser/factories.py
@@ -12,19 +12,10 @@ class PennUserFactory(factory.DjangoModelFactory):
     pennid = factory.fuzzy.FuzzyText(length=8, chars=string.digits)
 
 
-class PennUserStaffFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = PennUser
-    username = factory.Sequence(lambda n: "tjefferson{0}".format(n))
-    email = factory.LazyAttribute(lambda obj: "{0}@seas.upenn.edu".format(obj.username))
+class PennUserStaffFactory(PennUserFactory):
     is_staff = True
-    is_active = True
 
 
-class PennUserAdminFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = PennUser
-    username = factory.Sequence(lambda n: "tjefferson{0}".format(n))
-    email = factory.LazyAttribute(lambda obj: "{0}@seas.upenn.edu".format(obj.username))
-    is_staff = True
-    is_active = True
+class PennUserAdminFactory(PennUserStaffFactory):
     is_admin = True
 
